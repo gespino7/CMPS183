0b34f91146ecc4de6af93c23ce40b141bfb99cfb
jiayuancc <jiayuan_hu@hotmail.com>
2017-01-25 10:46:02 -0800

    add

--- Easycom/controllers/default.py
+++ Easycom/controllers/default.py
@@ -13,13 +13,13 @@ def index():
     """
     example action using the internationalization operator T and flash
     rendered by views/default/index.html or views/generic.html
-
+g
     if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
+    return augitth.wiki()
     """
     response.flash = T("Hello World")
 
-    return dict(message=T('Welcome to web2py!'))
+    return dict(message=T('Welcome to EasyCommerce'))
 
 
 def user():

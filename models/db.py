ed23da23f0a94560484e11b6a847be511e56f8bd
jiayuancc <jiayuan_hu@hotmail.com>
2017-02-24 10:01:47 -0800

    aniel

--- models/db.py
+++ models/db.py
@@ -3,54 +3,38 @@
 
 #DAL Constructor
 
-import datetime
-from gluon.tools import Auth
-db = DAL ('sqlite://storage.sqlite')
+db = DAL ('sqlite://storage.sqlite',migrate = False)
 
 from gluon.tools import Auth
 auth = Auth(db)
 auth.define_tables()
 #Table Constructor -> method to define new tables.
 #Vendor contact info.
-auth = Auth(db)
-auth.define_tables(username = False, signature = False)
-auth.settings.registration_requires_verification = False
-auth.settings.reset_password_requires_verification = True
-
-
 db.define_table('vendor',
-                Field('name'),
+                Field('name' ),
                 Field('email'),
                 Field('phone_num'),
                 Field('business_name'),
                 Field('address'),
                 Field('date','datetime'),
-                migrate=False,
                  format='%(business_name)s')
-
-
 
 #User contact info.
 db.define_table('user',
-                Field('name'),
+                Field('name' ),
                 Field('email'),
                 Field('phone_num'),
                 Field('payment_info'),
                 Field('date','datetime'),
-                migrate=False,
                  format='%(name)s')
 
 db.define_table('invoice',
-                Field('vendor_id','integer'),
-                Field('user_id','integer'),
-                Field('product_id','integer'),
+                Field('seller'),
+                Field('customer'),
                 Field('amount'),
-                Field('created_on','datetime',default=datetime.datetime.utcnow()),
+                Field('date','datetime',default=request.now),
                 Field('invoice'),
-                Field('status'),
-                migrate = False)
-
-
+                Field('status'))
 
 #Validations
 db.vendor.business_name.requires = IS_NOT_IN_DB(db, db.vendor.business_name)
@@ -74,5 +58,3 @@ db.user.payment_info.requires = IS_NOT_EMPTY()
 
 #auth.settings.extra_fields['auth_user']= [Field('phone')]
 auth.define_tables(username=False,signature=False,migrate =False)
-
-
